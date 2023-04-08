13b93cb6dd7971e0af998b6725558e35b0bf3505
ysnsales <ysnsaless@gmail.com>
2023-04-08 20:10:11 -0300

    Estrutura principal da API e rota GET tweets

--- src/app.js
+++ src/app.js
@@ -1,18 +1,19 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const PORT = 5000;
 
 app.listen(PORT);
+app.use(cors());
 
-const tweets = [
-	{
-		username: "bobesponja",
-		avatar: "https://cdn.shopify.com/s/files/1/0150/0643/3380/files/Screen_Shot_2019-07-01_at_11.35.42_AM_370x230@2x.png",
-		tweet: "Eu amo hambúrguer de siri!"
-	}
-]
+const tweets = []
 
 app.get(`/tweets`, (req, res) => {
+    if (tweets.length === 0){
+        res.send([])
+    }
+    else{
     res.send(tweets)
+    }
 })
