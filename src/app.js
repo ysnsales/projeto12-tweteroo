459ba4bc761789da307aa342c63feaaec230983f
ysnsales <ysnsaless@gmail.com>
2023-04-08 21:17:28 -0300

    Rota POST sign-up

--- src/app.js
+++ src/app.js
@@ -1,13 +1,17 @@
 import express from "express";
 import cors from "cors";
 
+//Criação do APP Servidor
 const app = express();
-const PORT = 5000;
 
+//Configurações do servidor
+const PORT = 5000;
 app.listen(PORT);
 app.use(cors());
+app.use(express.json())
 
-const tweets = []
+const tweets = [];
+const user = [];
 
 app.get(`/tweets`, (req, res) => {
     if (tweets.length === 0){
@@ -16,4 +20,11 @@ app.get(`/tweets`, (req, res) => {
     else{
     res.send(tweets)
     }
+});
+
+app.post(`/sign-up`, (req, res) => {
+    const {username, avatar} = req.body
+    const newUser = {username, avatar}
+    user.push(newUser);
+    res.send("OK");
 })
